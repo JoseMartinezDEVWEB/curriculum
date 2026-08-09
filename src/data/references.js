e80d975198d6495fc258ac644ac817799adb1caa
JoseMartinezDEVWEB <j.4developerweb19@gmail.com>
2026-08-09 15:10:44 -0400

    agregando datos a referencia..

--- src/data/references.js
+++ src/data/references.js
@@ -1,5 +1,5 @@
 export const references = [
   { name: 'Sr. Ismael Rodriguez', phone: '809-650-5555' },
   { name: 'Sra. Noemi Pinalez', phone: '809-252-9124' },
-  { name: 'Sr. Juan Carlos Piña', phone: '809-252-9124' },
+  { name: 'Sr. Juan Carlos Perez', phone: '809-252-9124' },
 ]
